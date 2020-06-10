e468bd23a625d32d63dcd498306e5793347c7274
Shogo Ogami <sg.tao.so@gmail.com>
2020-06-11 08:19:50 +0900

    add get text from editarea by split button

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,4 +1,12 @@
 'use strict';
 
+var textarea_source = document.querySelector('#editarea');
+var button_split = document.querySelector('#split');
+var source_text="foo"; 
 
-
+button_split.addEventListener('click', () => {
+  console.log(source_text);
+  source_text = textarea_source.value;
+  console.log(textarea_source);
+  console.log(source_text);
+});
