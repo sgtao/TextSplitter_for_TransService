c5818bb6d89322307ec7dab0f1fd2bb7a013d565
Shogo Ogami <sg.tao.so@gmail.com>
2020-06-13 00:04:24 +0900

    add a button for flush Result.

--- docs/js/main.js
+++ docs/js/main.js
@@ -2,8 +2,10 @@
 
 var textarea_source = document.querySelector('#editarea');
 var button_split = document.querySelector('#split');
+var button_flush = document.querySelector('#flush');
 var source_text=""; 
 var result_area = document.querySelector('#results');
+var initial_element = "";
 
 // subfunction for debug
 function show_charcode(textstring){
@@ -19,7 +21,7 @@ function split_textstrings(textstring) {
   return str_array; 
 }
 
-// add splited text by appendChild
+// add splited text to target element by appendChild
 function add_split_text(target, str) {
   let add_div = document.createElement('div');
   let add_hr = document.createElement('hr');
@@ -29,6 +31,7 @@ function add_split_text(target, str) {
   target.appendChild(add_hr);
 }
 
+// invoke split text
 button_split.addEventListener('click', () => {
   source_text = textarea_source.value;
   // console.dir(source_text);
@@ -39,6 +42,7 @@ button_split.addEventListener('click', () => {
   str_array = split_textstrings(source_text);
 
   // show sentenses at each textboxs
+  flush_Result_area();
   for (let i in str_array) {
     console.log(`${i} : ${str_array[i]}`);
     add_split_text(result_area, `${i} : ${str_array[i]}`);
@@ -46,4 +50,17 @@ button_split.addEventListener('click', () => {
 
 });
 
+// rewrite target by initial string
+function flush_Result_area() {
+  result_area.innerHTML = initial_element;
+}
+// invoke flush Result
+button_flush.addEventListener('click', () => {
+  flush_Result_area();
+});
+
+
+window.onload = () =>{
+  initial_element = result_area.innerHTML;
+}
 
