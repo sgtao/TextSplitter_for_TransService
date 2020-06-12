73870922a026b683bdcca3045be2b6cd37e2b36c
Shogo Ogami <sg.tao.so@gmail.com>
2020-06-13 02:13:40 +0900

    update next sentece condition (over twice newline)

--- docs/js/main.js
+++ docs/js/main.js
@@ -18,38 +18,53 @@ function show_charcode(textstring){
 function split_textstrings(textstring) {
   let str_array = [];
   let tmp_str = '';
-  let newline = true, next_sentence = false;
+  let head_sentence= true, next_sentence = false;
+  let cnt_newline = 0;
+
+  function update_next_sentence(str_array, tmp_str) {
+    str_array.push(tmp_str);
+    head_sentence = true;
+    next_sentence = false;
+    cnt_newline = 0;
+    return '';
+  }
+
   console.log("string : ", tmp_str);
-  for (let i = 0, newline = true, next_sentence = false; i < textstring.length; i++) {
+  for (let i = 0; i < textstring.length; i++) {
     let c_char = textstring.charAt(i);
     let c_code = textstring.charCodeAt(i);
     let n_code = textstring.charCodeAt(i+1);; // code of next charactor
     console.log(c_char, " : char code = ", c_code.toString(16));
 
     // 1文字ずつ「result」に格納していく
-    if (c_code === 0x0a) { continue; } // if newline, skip
-    if (c_code === 0x20 && newline === true) { continue; } // if space at head of sentence, skip
+    if (c_code === 0x0a) { 
+      if (cnt_newline > 2) { // newline twice, update sentence
+        tmp_str = update_next_sentence(str_array, tmp_str);
+      } else {
+        cnt_newline++;
+      }
+      continue;
+    } // if newline, skip
+    if (c_code === 0x20 && head_sentence === true) { continue; } // if space at head of sentence, skip
     tmp_str += c_char;
 
     // update newline flag
-    if (c_code === 0x2e && newline === true) { continue; } // if priod at head of sentence, no change newline flag }
-    if ((c_code >= 0x30 && c_code <= 0x39) && newline === true) { continue; } // if number at head of sentence, no change newline flag }
-    newline = false;
+    if (c_code === 0x2e && head_sentence === true) { continue; } // if priod at head of sentence, no change newline flag }
+    if ((c_code >= 0x30 && c_code <= 0x39) && head_sentence === true) { continue; } // if number at head of sentence, no change newline flag }
+    head_sentence = false;
 
     console.log(tmp_str);
 
     // if period , update next sentence.
-    if (c_code === 0x2e && newline === false) { 
+    if (c_code === 0x2e && head_sentence === false) { 
       // next is space or newline, update sentence
-      if (n_code === 0x20 || n_code === 0x0a) { next_sentence = true; }
+      if (n_code === 0x20 || n_code === 0x0a) { 
+        tmp_str =  update_next_sentence(str_array, tmp_str);
+      }
     }
-    if (c_code === 0x3b && newline === false) {
-      next_sentence = true;
-    }
-    if (next_sentence === true) {
-      str_array.push(tmp_str);
-      tmp_str = '';
-      newline = true;
+    // if double-quatation, update next sentence.
+    if (c_code === 0x3b && head_sentence === false) {
+      tmp_str = update_next_sentence(str_array, tmp_str);
     }
   }
 
